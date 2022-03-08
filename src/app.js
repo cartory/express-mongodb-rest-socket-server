d53bebd72342ce14b0dfec63e6bb6dafa0da0f67
pedro.caricari <pedro.caricari@bg.com.bo>
2022-03-08 16:47:19 -0400

    update app, index > add mongo-sockets

--- src/app.js
+++ src/app.js
@@ -2,27 +2,22 @@ const cors = require("cors")
 const express = require("express")
 const mongoose = require("mongoose")
 
-const mongoRoutes = require('./mongo/mongo.routes')
+const mongoRoutes = require("./mongo/mongo.routes")
 
 const app = express()
 
 mongoose
 	.connect(process.env.MONGOURI)
-	.then((m) => {
-		console.log("MONGO!!")
-		m.connection
-			.watch()
-			.on("init", (init) => console.log({ init }))
-			.on("change", (change) => console.log({ change }))
-			.on("more", (more) => console.log({ more }))
-	})
+	.then(() => console.log('MONGO!!'))
 	.catch((err) => console.error(err))
 
-app.use(cors())
+app
+	//
+	.use(cors())
 	.use(express.urlencoded({ extended: true }))
 	.use(express.json({ limit: process.env.BODY_SIZE }))
 	// ROUTES
 	.get("/", (_, res) => res.send("<h1>Welcome to API 😊</h1>"))
-	.use('/mongo', mongoRoutes)
+	.use("/mongo", mongoRoutes)
 
 module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-require('dotenv').config()
+require("dotenv").config()
 
-const app = require('./app')
+const app = require("./app")
+const mongo = require('./mongo/mongo.socket')
 
-const http = require('http')
-const socket = require('socket.io')
-const mongoose = require('mongoose')
+const http = require("http")
+const socket = require("socket.io")
 
 const server = http.createServer(app)
-// const io = new socket.Server(server)
 
-const delayInMilliseconds = 500
+const io = new socket.Server(server)
+
+io.on('connection', socket => {
+	console.log('new conection');
+	mongo.addSocket(socket)
+})
 
 server.listen(process.env.PORT, () => {
-    console.log(`Server running on \x1b[33mhttp://${process.env.HOST}:${process.env.PORT}\x1b[0m`)
+	console.log(`Server running on \x1b[33mhttp://${process.env.HOST}:${process.env.PORT}\x1b[0m`)
 	console.log(new Date())
 })

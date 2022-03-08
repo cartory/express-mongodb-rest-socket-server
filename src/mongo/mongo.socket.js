b1d15dd2faebeff7bd27e9d2ee769eafd357d204
pedro.caricari <pedro.caricari@bg.com.bo>
2022-03-08 18:03:37 -0400

    update mongo.socket

--- src/mongo/mongo.socket.js
+++ src/mongo/mongo.socket.js
@@ -1,34 +1,35 @@
 const socket = require("socket.io")
 const mongoose = require("mongoose")
 
-const collectionService = require('./collection.service')
+const collectionService = require("./collection.service")
 
 /**
  * add Socket according mongo events
  * @param {socket.Server} socket
  */
 const addSocket = (socket) => {
-    socket.on('init', () => {
-        collectionService.findAll()
-            .then(collections => {
-                socket.broadcast.emit('first-render', collections)
-            })
-            .catch(err => {
-                console.error(err)
-                socket.emit('error', err)
-            })
-    })
+	socket.on("init", () => {
+		collectionService
+			.findAll()
+			.then((collections) => {
+				socket.broadcast.emit("first-render", collections)
+			})
+			.catch((err) => {
+				console.error(err)
+				socket.emit("error", err)
+			})
+	})
 
 	mongoose.connection.watch().on("change", (change) => {
-        socket.broadcast.emit('render', {
-            ns: change.ns,
-            operationType: change.operationType,
+		socket.broadcast.emit("render", {
+			ns: change.ns,
+			operationType: change.operationType,
 
-            document: change.fullDocument,
-            documentKey: change.documentKey.id,
+			document: change.fullDocument,
+			documentKey: change.documentKey.id,
 
-            updateDescription: change.updateDescription,
-        })
+			updateDescription: change.updateDescription,
+		})
 	})
 }
 
